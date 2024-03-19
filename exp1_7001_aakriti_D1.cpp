1aff4d5a113ffb3fad8ee59531c2096536395c51
Aakriti <160565628+2310997001@users.noreply.github.com>
2024-03-19 20:10:26 +0530

    Create exp1_7001_aakriti_D1.cpp

---
+++ exp1_7001_aakriti_D1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main{
+int number, count=0;
+for(int i=0;i<5;i++){
+cout<<"enter the number"<endl;
+cin>>number;
+if(number%8==0)
+count++;
+}
+}
+  cout<<totol<<count<<"valid numbers"<<endl;
+
+return 0;
+}
